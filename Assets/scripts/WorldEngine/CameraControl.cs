ee23e0da79ae8de6e4a1e494fd4e22259358f24c
Alex Ethier <aethier@deliverr.com>
2022-02-13 22:11:06 -0500

    Added initial dual camera control.

--- Assets/scripts/WorldEngine/CameraControl.cs
+++ Assets/scripts/WorldEngine/CameraControl.cs
@@ -13,8 +13,15 @@ public class CameraControl : MonoBehaviour
     public float currentSize;
     float targetZoom;
 
+    private Transform baseTransform;
     private float camMaxSize;
     private float camMinSize;
+
+    private float slide;
+    private float width;
+    private float height;
+
+    private GameObject controlCamera;
 
     void OnGUI()
     {
@@ -44,6 +51,11 @@ public class CameraControl : MonoBehaviour
         camMinSize = 100F;
         camMaxSize = cam.orthographicSize;
         currentSize = cam.orthographicSize;
+        baseTransform = cam.transform;
+
+        this.slide = 0.8F;
+        this.width = cam.pixelRect.width;
+        this.height = cam.pixelRect.height;
     }
 
     private void AdjustCameraZoom(float amount) {
@@ -56,7 +68,48 @@ public class CameraControl : MonoBehaviour
             newSize = Mathf.MoveTowards(cam.orthographicSize, camMaxSize, -1 * speed * amount * Time.deltaTime);
             //Debug.Log("Zoom out new size: " + newSize);
         }
-        //Debug.Log("Old Size: " + cam.orthographicSize + " -> " + newSize);
+
+        if(currentSize == camMaxSize && newSize < currentSize) {
+            // Apply Camera Transform
+            this.AdjustCameraTransform(true);
+        } else if(newSize == camMaxSize && newSize > currentSize) {
+            // Unapply Camera Transform
+            this.AdjustCameraTransform(false);
+        }
         currentSize = newSize;
+    }
+
+    private void AdjustCameraTransform(bool isAdjust) {
+        int direction = -1;
+        if(isAdjust) {
+
+            // Create a second camera on the control panel
+            GameObject cameraGameObject = new GameObject();
+            cameraGameObject.transform.position = new Vector3(0F,0F,0F);
+            cameraGameObject.name = "Control Panel Camera";
+            Camera camera = cameraGameObject.AddComponent<Camera>();
+            camera.CopyFrom(this.cam);
+            camera.pixelRect = new Rect(0, 0, (1 - this.slide)*this.width, this.height);
+            camera.transform.SetParent(GameObject.Find("Control Panel").transform);
+            float z = cameraGameObject.transform.localPosition.z;
+            cameraGameObject.transform.localPosition = new Vector3(0,0,z);
+
+            cameraGameObject.SetActive(true);
+            this.controlCamera = cameraGameObject;
+            //
+
+            direction = 1;
+            cam.pixelRect = new Rect((1 - this.slide)*this.width, 0, this.slide*this.width, this.height);
+
+        } else {
+            // Destroy secondary camera
+            Destroy(this.controlCamera);
+            this.controlCamera = null;
+            //
+
+            cam.pixelRect = new Rect(0,0,this.width, this.height);
+
+        }
+        cam.transform.Translate(new Vector3(direction * 0.25F*(1 - this.slide)*this.width, 0, 0), Space.World);
     }
 }
